0b71f6b06097baa095317a393b06237a8579b743
Tenn456 <hiblubja@gmail.com>
2023-10-25 19:17:23 -0400

    +greetings.py +pluralize.py

---
+++ greetings.py
@@ -0,0 +1,4 @@
+def greet():
+	print("hello")
+for i in range(0, 5, 1):
+	greet()
---
+++ pluralize.py
@@ -0,0 +1,4 @@
+def pluralize(word):
+	return word + "s"
+userWord = input("Word please...: ")
+print(pluralize(userWord))
